7da12e4aa7ed7ae5588dc6f0299c65d0f9eab29f
4x2vk <kuandykkaiyrzhan@gmail.com>
2023-06-24 06:13:10 +0900

    Add flutter_svg library and add widget styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const FlutterTestApp());
@@ -10,12 +11,19 @@ class FlutterTestApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FlutterTest',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+        primarySwatch: Colors.yellow,
+        dividerColor: Colors.white,
+        appBarTheme: AppBarTheme(backgroundColor: Color.fromARGB(255, 31, 31, 31), titleTextStyle: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+        textTheme: TextTheme(
+          bodyMedium: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20),
+          labelSmall: TextStyle(color: Colors.white.withOpacity(0.6), fontWeight: FontWeight.w500, fontSize: 14),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Test'),
     );
   }
 }
@@ -30,39 +38,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView.separated(
+        itemCount: 10,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, i) => ListTile(
+          leading: SvgPicture.asset('assets/svg/bitcoinLogo.svg', height: 30, width: 30,),
+          title: Text('Bitcoin', style: theme.textTheme.bodyMedium),
+          subtitle: Text('200000\$', style: theme.textTheme.labelSmall),
+          trailing: const Icon(Icons.arrow_forward_ios),
+        ), 
       ),
     );
   }
